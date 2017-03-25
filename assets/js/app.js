41ebca48f49adbfbb926f82a334c3dd0c035a03f
Ana Ausek <anac_ausek@hotmail.com>
2017-03-25 09:11:05 -0400

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -119,6 +119,7 @@ $(document).ready(function() {
       .done(function(response) {
         // Log the resulting object
         console.log(response);
+        
     $("#colorado").click(function() {
       $(".weatherinfo").append("<h1>" + response.name + " Weather Details</h1>");
       $(".weatherinfo").append("<h3> Temperature (F) " + response.main.temp + "</h3>");
